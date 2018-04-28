fe431a570e102142955c4e6bc4135415df8c38e3
pangmengchen <mengchen_0212@foxmail.com>
2018-04-28 15:04:09 +0800

    用户查询

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ require('./connectDB')
 module.exports = function (app) {
   // baseAPI
   require('./base')(app)
-  app.use('/user', require(CONTROLLERS + '/user'))
-  app.use('/question', require(CONTROLLERS + '/question'))
-  app.use('/answer', require(CONTROLLERS + '/answer'))
+  app.use('/api/user', require(CONTROLLERS + '/user'))
+  app.use('/api/question', require(CONTROLLERS + '/question'))
+  app.use('/api/answer', require(CONTROLLERS + '/answer'))
 }
